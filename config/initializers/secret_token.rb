6dced28b0c424fdb79b588f7641cebe5c04ff276
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-02 18:36:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlyfreeMe::Application.config.secret_key_base = 'a2e7e594ab79cdf4d45d59c7376485e153ddbac78469915c6a89d6e1a3535b2b4b29d2bbd86eaf6f1d3178441e96f7cfba6f499a698b79b1a4bb480ef323782a'
