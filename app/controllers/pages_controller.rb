f03d6859764e8f7bb067a2781ae26b726eab65b8
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-05 08:15:34 -0600

    Working on embedding videos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def index
+  	@videos = Video.all
   end
 
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,8 @@
 class Video < ActiveRecord::Base
   validates_presence_of :title, :url, :description
+
+  def self.yt_video_id
+  	regex = /youtube.com.*(?:\/|v=)([^&$]+)/
+    video.url.match(regex)
+  end
 end
