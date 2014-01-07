bf37c3c36b5ac3a435c7981917567a4eb9c3e61e
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-06 20:11:19 -0600

    Refactored user tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FlyfreeMe::Application.routes.draw do
   root 'pages#index'
+  get 'register', to: "users#new"
+  get 'sign_in', to: "sessions#new"
+  get 'sign_out', to: "sessions#destroy"
 
   resources :videos
   resources :users, except: [:destroy]
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,30 +10,29 @@ describe UsersController do
 
 	describe "POST create" do
 		context "with valid input" do
+			before { post :create, user: Fabricate.attributes_for(:user) }
+
 			it "creates the user" do
-				post :create, user: Fabricate.attributes_for(:user)
 				expect(User.count).to eq(1)
 			end
 
 			it "redirects to root path" do
-				post :create, user: Fabricate.attributes_for(:user)
 				expect(response).to redirect_to root_path
 			end
 		end
 
 		context "with invalid input" do
+			before { post :create, user: { password: "password", username: "Jack" } }
+
 			it "does not create the user" do
-				post :create, user: { password: "password", username: "Jack" }
 				expect(User.count).to eq(0)
 			end
 
 			it "renders the :new template" do
-				post :create, user: { password: "password", username: "Jack" }
 				expect(response).to render_template :new
 			end
 
 			it "sets @user" do
-				post :create, user: { password: "password", username: "Jack" }
 				expect(assigns(:user)).to be_instance_of(User)
 			end
 		end
