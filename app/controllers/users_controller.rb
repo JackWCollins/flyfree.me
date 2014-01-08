b479687284df58babb3e370a955ff881c44d7876
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-07 20:49:38 -0600

    Show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
 	def user_params
 		params.require(:user).permit(:full_name, :password, :email, :username)
 	end
