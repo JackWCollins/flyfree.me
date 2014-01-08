9aac737ccaf3fe44941574dd20e85acdec965864
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-07 20:18:09 -0600

    Sign In/Sign Out for header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
-
-  def require_user
-  	redirect_to sign_in_path unless current_user
-  end
+  helper_method :current_user, :logged_in?
 
   def current_user
-  	User.find(session[:user_id]) if session[:user_id]
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+  	!!current_user
+  end
+
+  def require_user
+  	if !logged_in?
+  		flash[:notice] = "Please sign in!"
+  		redirect_to sign_in_path
+  	end
   end
 end
