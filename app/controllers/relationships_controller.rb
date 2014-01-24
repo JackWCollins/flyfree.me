f2cd5054beb8a0eb287a0a5d0d8749142b32936b
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-23 20:56:30 -0600

    Finished following functionality

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,7 +12,7 @@ class RelationshipsController < ApplicationController
 	end
 
 	def destroy
-		relationship = Relationship.find(params[:id])
+		relationship = Relationship.find_by_leader_id(params[:leader_id])
 		leader = relationship.leader
 		relationship.destroy if relationship.follower == current_user
 		redirect_to user_path(leader)
