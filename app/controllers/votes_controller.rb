c7d8f49ac627b180c32087607d6fb3f51b557bf6
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-25 16:43:06 -0600

    Implemented voting in the views

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,7 @@ class VotesController < ApplicationController
 
 	def create
 		@video = Video.find(params[:video_id])
-		Vote.create(vote: true, user: current_user)
+		Vote.create(vote: params[:vote], user: current_user, video_id: @video.id)
 		redirect_to root_path
 	end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :video
 
-	validates_uniqueness_of :user
+	validates_uniqueness_of :user, scope: :video
 end
