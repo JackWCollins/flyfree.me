4b6fa0565e45720004f9971550ac5295666938b5
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-30 15:29:26 -0600

    Finally got AJAX voting working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,16 @@
 class VotesController < ApplicationController
-	before_action :require_user
 
 	def create
-		@video = Video.find(params[:video_id])
-		@vote = Vote.create(vote: params[:vote], user: current_user, video_id: @video.id)
-
 		respond_to do |format|
-			format.html { redirect_to @video, notice: "Vote was counted" }
-			format.js {}
+			if logged_in?
+				@video = Video.find(params[:video_id])
+		    @vote = Vote.create(vote: params[:vote], user: current_user, video_id: @video.id)
+			  format.html { redirect_to @video, notice: "Vote was counted" }
+			  format.js {}
+			else
+				format.html { redirect_to sign_in_path }
+				format.js {}
+			end
 		end
 	end
 end
