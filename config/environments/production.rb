be1072ea9c0624faf901185dc7c3bb8a0be66e41
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-01-30 11:28:58 -0600

    Mailgun setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 FlyfreeMe::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
