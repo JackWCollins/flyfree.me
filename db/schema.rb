c450a547b3afd89ce165e493a12c46fe64a38b1f
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-02-01 14:09:19 -0600

    Added video feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201191720) do
+ActiveRecord::Schema.define(version: 20140201200656) do
 
   create_table "relationships", force: true do |t|
     t.integer  "leader_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140201191720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "thumbnail_url"
+    t.boolean  "featured"
   end
 
   create_table "votes", force: true do |t|
