bbc91dd3cd7d069edc51cb9590099980fe445596
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-02-04 19:18:00 -0600

    Page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
 	def options_for_video_reviews(selected=nil)
 		options_for_select([5,4,3,2,1].map{ |number| [pluralize(number, "Star"), number]}, selected)
 	end
+
+	def title(page_title)
+		content_for(:title) { page_title }
+	end
 end
