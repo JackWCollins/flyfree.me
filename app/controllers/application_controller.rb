c1b7d4981d9d71a6469cc66dda41618c64f36960
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-03-23 20:48:36 -0500

    Delete video for featured videos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin?
 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
   	!!current_user
+  end
+
+  def admin?
+    !!(logged_in? && current_user.admin?)
   end
 
   def require_user
